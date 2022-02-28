9645b2a21c8b914a858a575f9dba77e10276feef
Braden Carlson <devbdaily@gmail.com>
2022-02-27 23:00:00 -0700

    Update form validation error flashing

--- resources/views/movies/form.blade.php
+++ resources/views/movies/form.blade.php
@@ -9,9 +9,15 @@
             {{ $success }}
         @endisset
 
+        @error('title')
+            {{ $message }}
+        @enderror
         <label for="title">Title:</label>
         <input id="title" name="title" type="text" maxlength="50" value="{{ isset($movie) ? $movie->title : old('title') }}">
 
+        @error('format')
+            {{ $message }}
+        @enderror
         <label for="format">Format</label>
         <select id="format" name="format">
             <option value="VHS"{{ isset($movie) && $movie->format == 'VHS' ? ' selected' : '' }}>VHS</option>
@@ -19,12 +25,21 @@
             <option value="Streaming"{{ isset($movie) && $movie->format == 'Streaming' ? ' selected' : '' }}>Streaming</option>
         </select>
 
+        @error('length')
+            {{ $message }}
+        @enderror
         <label for="length">Length</label>
         <input id="length" name="length" type="number" min="1" max="500" value="{{ isset($movie) ? $movie->length : old('length') }}">
 
+        @error('release_date')
+            {{ $message }}
+        @enderror
         <label for="release_date">Release Date</label>
         <input id="release_date" name="release_date" type="text" value="{{ isset($movie) ? $movie->release_date->format('m/d/Y') : old('release_date') }}">
 
+        @error('rating')
+            {{ $message }}
+        @enderror
         <label for="rating">Rating</label>
         <input id="rating" name="rating" type="number" min="1" max="5" value="{{ isset($movie) ? $movie->rating : old('rating') }}">
 
